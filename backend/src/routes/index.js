dc325b9d5f96b9d9c1e0b60b8ebd21879986d5f5
Kuruv Patel <kuruvpatel2005@gmail.com>
2025-02-01 16:00:05 +0530

    backend auth

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,11 @@
 const { Router } = require("express");
 const errorHandler = require("strong-error-handler");
 
+const authRoute = require("./auth.route");
+
 const router = Router();
+
+router.use("/auth", authRoute);
 
 /**
  * GET /health
